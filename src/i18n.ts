e12533c93a1f801d0bd6b7196ad3d4372eb3cac7
martingadea12 <martiingadeea1996@gmail.com>
2025-11-10 10:39:46 -0300

    Some corrections

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 // src/i18n.ts
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector' 
 
 /**
  * Recursos ES/EN completos y deduplicados.
